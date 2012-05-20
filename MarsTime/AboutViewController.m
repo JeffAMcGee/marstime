e2c0aaa9e47396fc0101f54c8bae7bcfe4d99ce7
Jeff McGee <JeffAMcGee@gmail.com>
2012-05-20 15:09:25 -0500

    cleaned up copyright statements

--- MarsTime/AboutViewController.m
+++ MarsTime/AboutViewController.m
@@ -1,9 +1,8 @@
-//
 //  SecondViewController.m
 //  MarsTime
 //
 //  Created by Jeffrey McGee on 5/20/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012 Jeffrey McGee. Licensed under the MIT license.
 //
 
 #import "AboutViewController.h"
